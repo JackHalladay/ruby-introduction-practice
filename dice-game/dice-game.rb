d73f33e579a1a236ee3b9dda955a61e36ee84930
Jack <jackhalladay@gmail.com>
2021-07-08 15:17:23 +0100

    Players now have a combined score from 2 dice rolls

--- dice-game/dice-game.rb
+++ dice-game/dice-game.rb
@@ -1,9 +1,9 @@
 roll = 7
-player1 = rand(roll)
-player2 = rand(roll)
-puts "PLAYER 1'S ROLL IS..."
+player1 = rand(roll) + rand(roll)
+player2 = rand(roll) + rand(roll)
+puts "PLAYER 1'S COMBINED ROLL IS..."
 puts player1
-puts "PLAYER 2'S ROLL IS..."
+puts "PLAYER 2'S COMBINED ROLL IS..."
 puts player2
 if player1 > player2
   puts "I win"
